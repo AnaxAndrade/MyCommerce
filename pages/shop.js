2fd7c3ad2ea767f87d20fcbaa67ee080d155c274
Anaximandro Andrade <anaxfive@hotmail.com>
2021-11-04 18:15:35 -0100

    Implementada pesquisa por produtos na loja

--- pages/shop.js
+++ pages/shop.js
@@ -3,15 +3,27 @@ import { Fragment } from 'react'
 import ProductList from '../components/products/product-list'
 import Link from 'next/link';
 import Head from 'next/head'
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import fetcher from "../lib/fetcher";
 import useSWR from "swr";
 
 export default function Shop(){
+    const searchRef = useRef("");
+
+    const pageSize = 8;
     
     const { data, error } = useSWR('/api/products?limit=100', fetcher);
     const [produtos, setProdutos] = useState([]);
+    const [searchTerm, setSearchTerm] = useState("");
     const [pagePointer, setPagePointer] = useState(0);
+
+    function clear(){
+        searchRef.current.value = "";
+        setSearchTerm("");
+    }
+    function search(){
+        setSearchTerm(searchRef.current.value);
+    }
 
     function loadMore(){
         if (!data || error)
@@ -24,16 +36,35 @@ export default function Shop(){
         setPagePointer(ate);
     }
 
-    const pageSize = 8;
-
     useEffect(()=>{
         if (data)
         {
             if (pagePointer == 0) loadMore();
+
+            //Filtrar os produtos de acordo com o termo pesquisado
+            let filtered = data;
+            if (searchTerm && searchTerm.trim().length > 0)
+            {
+                filtered = data.filter((x) => {
+                    let terms = searchTerm.split(" ");
+                    for (let i = 0; i < terms.length; i++) {
+                    // Descartar a opção caso não contenha uma das palavras da busca
+                    if (
+                        (x.nome || "").toLowerCase().indexOf((terms[i] || "").toLowerCase()) <
+                        0
+                    ) {
+                        return false;
+                    }else{
+                        return true;
+                    }
+                    } 
+                });
+            }
+
             //console.log(pagePointer+"/"+data.length);
-            setProdutos(data.slice(0, pagePointer));
+            setProdutos(filtered.slice(0, pagePointer));
         }
-    },[data, pagePointer]);
+    },[data, pagePointer, searchTerm]);
 
     return (
         <Layout>
@@ -46,8 +77,9 @@ export default function Shop(){
                         <div className="container">
                             <div className="col-md-6 offset-md-3">
                                 <div className="input-group mb-3">
-                                    <input type="text" className="form-control" placeholder="Pesquisar Produto" aria-label="Pesquisar Produto" aria-describedby="button-addon2" />
-                                    <button className="btn btn-outline-primary" type="button" id="button-addon2">Pesquisar</button>
+                                    <input ref={searchRef} type="text" className="form-control" placeholder="Pesquisar Produto" aria-label="Pesquisar Produto" aria-describedby="button-addon2" />
+                                    <button className="btn btn-outline-primary" type="button" id="button-addon2" onClick={search}>Pesquisar</button>
+                                    <button className="btn btn-outline-primary" type="button" id="button-addon2" onClick={clear}>Limpar</button>
                                 </div>
                             </div>
                             <br/>
