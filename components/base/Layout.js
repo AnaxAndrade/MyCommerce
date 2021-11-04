b8bc38212ae889af78a7cbf400b951663a1b5661
Anaximandro Andrade <anaxfive@hotmail.com>
2021-11-04 04:35:25 -0100

    Adiconado FavIcon

--- components/base/Layout.js
+++ components/base/Layout.js
@@ -10,7 +10,7 @@ function Layout(props){
             <div className="container">
                 <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
                     <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-                        <img width="32" height="32" src="CART.png"/>
+                        <img width="32" height="32" src="/CART.png"/>
                         <span className="fs-4">My Commerce</span>
                     </a>
                     <ul className="nav nav-border col-12 col-md-auto mb-2 justify-content-center mb-md-0">
@@ -36,12 +36,14 @@ function Layout(props){
                         </li>
                     </ul>
                     <div className="col-md-3 text-end">
-                        <button type="button" className="btn btn-outline-primary me-2">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
-                            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-                        </svg>
-                        </button>
-                        <button type="button" className="btn btn-primary">Entrar</button>
+                        <Link href="/cart">
+                            <a className="btn btn-outline-primary me-2">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart3" viewBox="0 0 16 16">
+                                    <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+                                </svg>
+                            </a>
+                        </Link>
+                        <Link href="/login"><a className="btn btn-primary">Entrar</a></Link>
                     </div>
                 </header>
             </div>
---
+++ pages/account.js
@@ -0,0 +1,9 @@
+import Layout from "../components/base/Layout";
+
+export default function Cart(){
+    return (
+        <Layout>
+            <h3>Conta</h3>
+        </Layout>
+    );
+}
