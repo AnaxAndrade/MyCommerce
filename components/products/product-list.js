31c5afc4b3bfcf8b4aced0945bfab11e5ba84745
Anaximandro Andrade <anaxfive@hotmail.com>
2021-11-04 21:46:21 -0100

    Ajustado item produto para possibilitar click em Add to Cart

--- components/products/product-list.js
+++ components/products/product-list.js
@@ -1,9 +1,13 @@
 import Item from "./item";
 
 export default function ProductList(props){
+    function addToCardHandler(p){
+        props.onAddToCart(p);
+    }
+
     return(
         <div className="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3">
-            {props.items.map((i)  => <Item key={i.id} item={i}></Item>)}
+            {props.items.map((i)  => <Item key={i.id} item={i} onAddToCart={addToCardHandler}></Item>)}
         </div>
     );
 }
